9718e2a61955938c1b73a7cf91db9ea05a561d97
mlwebdev-react <mlwebdev-react@outlook.com>
2017-12-26 17:14:47 -0500

    Create and export fetchPost actionCreator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const FETCH_POSTS = "fetch_posts";
+export const FETCH_POST = "fetch_post";
 export const CREATE_POST = "create_post";
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
@@ -20,8 +21,18 @@ export function createPost(values, callback) {
     .post(`${ROOT_URL}/posts${API_KEY}`, values)
     .then(() => callback());
 
+  return {
+    type: CREATE_POST,
+    payload: request
+  };
+}
+
+export function fetchPost(id) {
+  const request = axios
+    .get(`${ROOT_URL}/posts${id}${API_KEY}`);
+
     return {
-      type: CREATE_POST,
+      type: FETCH_POST,
       payload: request
     };
   }
