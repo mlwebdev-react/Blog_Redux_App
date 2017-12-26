0cbec96dce84eb98002e93229c3795498cdaa8b1
mlwebdev-react <mlwebdev-react@outlook.com>
2017-12-26 11:40:47 -0500

    Add Switch to react-router-dom

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
 import reducers from './reducers';
