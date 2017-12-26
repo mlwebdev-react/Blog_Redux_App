4d00a633d7894b746e049d451737da7e25daf5e7
mlwebdev-react <mlwebdev-react@outlook.com>
2017-12-26 16:10:31 -0500

    Remove console.log ad wire up the submit to make POST request. Import and use connect method from react-redux.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
 
@@ -24,7 +26,7 @@ class PostsNew extends Component {
   }
 
   onSubmit(values) {
-    console.log(values);
+    this.props.createPost(values);
   }
 
   render() {
@@ -80,4 +82,4 @@ function validate(values) {
 export default reduxForm({
   validate,
   form: 'PostsNewForm'
-})(PostsNew);
+})(connect(null, { createPost })(PostsNew));
