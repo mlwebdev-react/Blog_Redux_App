00b066a0e1377cba06baa47bc6b950be0ebcc2e3
mlwebdev-react <mlwebdev-react@outlook.com>
2017-12-25 21:59:33 -0500

    Code clean up and add PostsIndex component

---
+++ src/components/posts_index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostIndex {
+  render() {
+    return (
+      <div>
+        Posts Index
+      </div>
+    );
+  }
+}
+
+export default PostIndex;
