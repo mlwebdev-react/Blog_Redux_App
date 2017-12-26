a60bcf81bf687b86cf13f5127b27d295aa6d4d17
mlwebdev-react <mlwebdev-react@outlook.com>
2017-12-26 11:24:01 -0500

    Create PostsNew component

---
+++ src/components/posts_new.js
@@ -0,0 +1,12 @@
+import React, {Compnent} from 'react';
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div className="class-name">
+      </div>
+    );
+  }
+}
+
+export default PostsNew;
